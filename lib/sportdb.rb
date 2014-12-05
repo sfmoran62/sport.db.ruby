5d9e9f5a41485f029c5f8a5edbbb84ca2cc3151a
Gerald Bauer <gerald.bauer@gmail.com>
2014-12-05 11:56:49 +0100

    prep 1.10.1

--- lib/sportdb.rb
+++ lib/sportdb.rb
@@ -3,6 +3,8 @@
 
 require 'sportdb/models'
 
+require 'logutils/activerecord'   ## NOTE: check - add to/include in/move to sportdb/models
+
 ## check if already included in datafile gem ??
 require 'fetcher'   # for fetching/downloading fixtures via HTTP/HTTPS etc.
 require 'datafile'   ## lets us use Datafile::Builder,Datafile etc.
--- lib/sportdb/cli/version.rb
+++ lib/sportdb/cli/version.rb
@@ -7,7 +7,7 @@ module SportDbCli    # todo/check - rename to SportDbTool or SportDbCommands or 
 
   MAJOR = 1 ## todo: namespace inside version or something - why? why not??
   MINOR = 10
-  PATCH = 0
+  PATCH = 1
   VERSION = [MAJOR,MINOR,PATCH].join('.')
 
   def self.version
